7918800b6288b7bd0a013f34c52577672f3aaac8
Marcin Krzeminski <krzemo89@hotmail.com>
2011-03-10 14:12:29 +0100

    przenioslem kontrolke logowania na strone glowna, dodalem funkcje wylogowania oraz kontrolke login name

--- KS/MasterPage.master.cs
+++ KS/MasterPage.master.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+            MembershipUser user = Membership.GetUser();
+            if (user!=null)
+            {
+                Login1.Visible = false;
+                LoginStatus1.Visible = true;
+            }
     }
 }
